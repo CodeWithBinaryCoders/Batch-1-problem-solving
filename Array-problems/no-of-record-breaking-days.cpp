71907c741b577a3ae11a6d571f6facdf82db60a4
Alik Agarwala <alikagarwala@outlook.com>
2024-04-19 23:40:37 +0530

    record breaker

---
+++ Array-problems/no-of-record-breaking-days.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    if (n == 1)
+    {
+        cout << "1";
+        return 0;
+    }
+
+    int count = 0;
+    int maximum = INT_MIN;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > maximum && arr[i] > arr[i + 1])
+            count++;
+        
+        maximum = max(maximum, arr[i]);
+    }
+
+    cout << count;
+
+    return 0;
+}
